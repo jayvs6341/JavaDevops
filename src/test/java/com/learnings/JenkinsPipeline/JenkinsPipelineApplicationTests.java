c0f1ccc0d52d3094c61c989bfafd16c7f09ee797
UnicodeINC opc <unicodeincopc@192.168.1.9>
2025-04-10 11:23:56 +0530

    init

---
+++ src/test/java/com/learnings/JenkinsPipeline/JenkinsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnings.JenkinsPipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
